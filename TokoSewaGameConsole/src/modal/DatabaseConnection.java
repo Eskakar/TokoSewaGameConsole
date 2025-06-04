113c168f1d70f06489e06a29749b1a2e1a207b2f
Eskakar <nabilaqilaputra89@gmail.com>
2025-06-04 21:18:44 +0700

    kosongan

---
+++ TokoSewaGameConsole/src/modal/DatabaseConnection.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modal;
+
+/**
+ *
+ * @author ASUS
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    private static Connection conn;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/TokoSewaGameConsole";
+                String user = "root";
+                String pass = "";
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection(url, user, pass);
+                System.out.println("Connected to DB");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return conn;
+    }
+}
+
